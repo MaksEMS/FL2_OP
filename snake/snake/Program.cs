717ed6f5aaee91243a30a187600ddc6003150d10
MaksEMS <maks.ems@mail.ru>
2017-02-08 22:41:09 +0600

    snake projekt

---
+++ snake/snake/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor {11111f,11111f}
+            readln;
+        }
+    }
+}
