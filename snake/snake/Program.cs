506e91bec4289048c0b66c0a8ee86b34e3085fa9
MaksEMS <maks.ems@mail.ru>
2017-02-19 13:25:22 +0600

    List with char and List width 4point

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,11 +10,19 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1,3,'*');
-            p1.Draw();
+            Point p1 = new Point(4,4,'*');
+          //  p1.Draw();
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            Point p2 = new Point(6, 6, '#');
+           // p2.Draw();
+
+            Point p3 = new Point(8, 8, '$');
+           // p3.Draw();
+
+            Point p4 = new Point(10, 10, '&');
+           // p4.Draw();
+
+            Console.WriteLine();
 
             List<int> numList = new List<int>();
             numList.Add(0);
@@ -25,16 +33,35 @@ namespace snake
             int y = numList[1];
             int z = numList[2];
 
+            numList.RemoveAt(0);
+
             foreach (int i in numList)
             {
                 Console.WriteLine(i);
             }
 
-            numList.RemoveAt(0);
+
+            List<char> myNameList = new List<char>();
+            myNameList.Add('M');
+            myNameList.Add('A');
+            myNameList.Add('K');
+            myNameList.Add('S');
+
+            foreach (char i in myNameList)
+            {
+                Console.WriteLine(i);
+            }
 
             List<Point> pList = new List<Point>();
             pList.Add(p1);
             pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+            foreach (Point i in pList)
+            {
+                i.Draw();
+            }
 
             Console.ReadLine();
         }
