87287d957c0b1f84af95108ece6d3ea313047cc2
MaksEMS <maks.ems@mail.ru>
2017-02-20 21:13:15 +0600

    Nasled

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,17 +10,27 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(4,4,'*');
-            p1.Draw();
+            Console.SetBufferSize(80, 25);
 
-            Point p2 = new Point(6, 6, '#');
-            p2.Draw();
+            //Отрисовка рамочки
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '#');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '#');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '#');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '#');
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
 
-            HorizontalLine lineH = new HorizontalLine(5,10,8,'+');
-            lineH.DrowH();
 
-            VerticalLine lineV = new VerticalLine(2, 7, 7, '$');
-            lineV.DrowV();
+            //Отрисовка точек
+            Point p = new Point(4,5,'*');
+            p.Draw();
+
+            Emploee e = new Emploee();
+            e.age = 27;
+            e.pay = 30000;
+            Console.WriteLine("Name MAKS, Age " + e.age + "ZP" + e.pay);
 
             Console.ReadLine();
         }
--- snake/snake/VerticalLine.cs
+++ snake/snake/VerticalLine.cs
@@ -6,26 +6,20 @@ using System.Threading.Tasks;
 
 namespace snake
 {
-    class VerticalLine
+    class VerticalLine : Figure
     {
-        List<Point> pList;
+        
 
-        public VerticalLine(int yTop, int yDown, int x, char sym)
+        public VerticalLine(int yUp, int yDown, int x, char sym)
         {
             pList = new List<Point>();
-            for (int y = yTop; y <= yDown; y++)
+            for (int y = yUp; y <= yDown; y++)
             {
                 Point p = new Point(x, y, sym);
                 pList.Add(p);
             }
         }
 
-        public void DrowV()
-        {
-            foreach (Point p in pList)
-            {
-                p.Draw();
-            }
-        }
+       
     }
 }
