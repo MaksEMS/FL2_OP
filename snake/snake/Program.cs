3cf7e97c218407d9ca9b14b30744b1e26d088e89
MaksEMS <maks.ems@mail.ru>
2017-02-25 11:56:58 +0600

    HitCheck

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -13,16 +13,8 @@ namespace snake
         {
             Console.SetBufferSize(80, 25);
 
-            //Отрисовка рамочки
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '#');
-            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '#');
-            VerticalLine leftLine = new VerticalLine(0, 24, 0, '#');
-            VerticalLine rightLine = new VerticalLine(0, 24, 78, '#');
-            upLine.Drow();
-            downLine.Drow();
-            leftLine.Drow();
-            rightLine.Drow();
-
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
 
             //Отрисовка точек
             Point p = new Point(4,5,'*');
@@ -36,6 +28,11 @@ namespace snake
 
             while (true)
             {
+                if(walls.IsHit (snake)||snake.IsHitTail())
+                {
+                    break;
+                }
+
                 if(snake.Eat (food))
                 {
                     food = foodCreator.CreateFood();
